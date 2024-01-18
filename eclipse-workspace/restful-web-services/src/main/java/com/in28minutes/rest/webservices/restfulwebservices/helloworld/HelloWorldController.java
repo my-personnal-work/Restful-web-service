a8089b0096778101f49d87966e6124624841b0b4
Ismael Seck <ismaelseck11@gmail.com>
2024-01-18 22:34:33 +0000

    Enhancing the Hello world Rest API to return a been

--- eclipse-workspace/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ eclipse-workspace/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,16 +1,22 @@
 package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 //REST API
 @RestController
 public class HelloWorldController {
 	
-	@RequestMapping(method = RequestMethod.GET, path= "/hello-world")
+	//@RequestMapping(method = RequestMethod.GET, path= "/hello-world")
+	@GetMapping(path="/hello-world")
 	public String HelloWorld() {
 		return "Hello World";
 	}
+	
+	@GetMapping(path="/hello-world-bean")
+	public HelloWorldBean HelloWorldBean() {
+		return new HelloWorldBean("Hello World");
+	}
+	
 
 }
